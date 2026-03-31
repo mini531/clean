29f2f76d0892adaae6c3a44005a9d31db81a2da4
sangmi shin <mini5031@nate.com>
2026-03-31 23:33:57 +0900

    Keep polygon visible when closing popup

--- js/map.js
+++ js/map.js
@@ -198,7 +198,7 @@ const MapModule = {
       <div class="dg-popup-container">
         <div class="dg-popup-header">
           <div class="dg-popup-title ${typeClass}">${typeLabel}(${pct})</div>
-          <button class="dg-popup-close" onclick="MapModule.clearHighlights()">
+          <button class="dg-popup-close" onclick="if(MapModule.highlightLayer) MapModule.highlightLayer.closePopup()">
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <line x1="18" y1="6" x2="6" y2="18"></line>
               <line x1="6" y1="6" x2="18" y2="18"></line>
